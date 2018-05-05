34731d797f6d13f79006aa3afe185d0d22727af6
Flaque <econrad@zagmail.gonzaga.edu>
2018-05-04 19:15:29 -0700

    WIP hbs template

--- src/webpack-plugin/index.js
+++ src/webpack-plugin/index.js
@@ -3,26 +3,36 @@
 const path = require("path");
 const { renderToString } = require("react-dom/server");
 const React = require("react");
+const fs = require("fs");
+const handlebars = require("handlebars");
 
 const flatten = arrs => [].concat.apply([], arrs);
 
 class HobkinPlugin {
+  constructor() {
+    // TODO: Switch to async
+    this.tmpl = fs.readFileSync("index.html.hbs");
+  }
+
   apply(compiler) {
-    compiler.hooks.emit.tap("HobkinPlugin", comp => {
+    compiler.hooks.afterEmit.tap("HobkinPlugin", comp => {
       const files = flatten(comp.chunks.map(c => c.files)).map(f =>
         path.resolve(comp.compiler.outputPath, f)
       );
 
-      if (!files || files.length === 0) {
-        return;
-      }
-
-      // TODO: Check if chunk is loaded before we require.
-
+      // TODO: work with other files
       const Component = require(files[0]).default;
       const str = renderToString(React.createElement(Component));
 
-      // TODO Add HTML asset here.
+      fs.writeFile(
+        path.resolve(comp.compiler.outputPath, "index.html"),
+        str,
+        err => {
+          if (err) {
+            console.log(err);
+          }
+        }
+      );
     });
   }
 }
